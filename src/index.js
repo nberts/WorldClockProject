0622278750a59326c855edd56da05d5f851780e9
nberts <nina@ninabsavage.ca>
2024-08-28 18:08:04 -0400

    One city accurate timezone added.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("hello world!");
+let parisTime = "Europe/Paris";
+
+function updateTime() {
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  parisDateElement.innerHTML = moment().tz(parisTime).format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = moment()
+    .tz(parisTime)
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
